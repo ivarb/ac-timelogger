50a04fe6586fa872add64b8834571e4c31ceed02
Ivar van der Burg <ivarb@angrybytes.com>
2012-05-10 12:03:56 +0200

    Remove sample

--- config.sample.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Api secrets
-$api = array(
-    'key'     => '{user-api-key}', // See active collab > user settings > api
-    'url'     => 'http://activecollab.example.com/api.php',
-    'user_id' => {userid} // See active collab > user settings
-);
