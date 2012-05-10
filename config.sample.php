1226c4fae8c18153e2b58f5a1a8bb953c8cc116f
Ivar van der Burg <ivarb@angrybytes.com>
2012-05-10 12:00:47 +0200

    Config sample

---
+++ config.sample.php
@@ -0,0 +1,7 @@
+<?php
+// Api secrets
+$api = array(
+    'key'     => '173-AqNcjUxOR6IHiIBq5CM0JlyqY8BBOBKRd7ezBDYR',
+    'url'     => 'http://projects.angrybytes.com/public/api.php',
+    'user_id' => 173
+);
