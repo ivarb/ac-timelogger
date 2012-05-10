762fb65aa2058b3dfeaafd799538134130446a4a
Ivar van der Burg <ivarb@angrybytes.com>
2012-05-10 12:04:12 +0200

    Added sample config

---
+++ config.sample.php
@@ -0,0 +1,7 @@
+<?php
+// Api secrets
+$api = array(
+    'key'     => '{user-api-key}', // See active collab > user settings > api
+    'url'     => 'http://activecollab.example.com/api.php',
+    'user_id' => {userid} // See active collab > user settings
+);
