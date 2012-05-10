3b372c7d8aaff810074c996fdf91e2a970210bca
Ivar van der Burg <ivarb@angrybytes.com>
2012-05-10 15:23:04 +0200

    Alert fadeout

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,14 @@
 // On ready
 $(document).ready(function() {
     try {
+        // Form submit button
         $('#submit').click(function(e) {
             e.preventDefault();
             $("#log").submit();
         });
+
+        // Time hide alert
+        setTimeout("$('.alert-success').fadeOut('slow')", 1000);
     } catch (e) {
         // BooBoo :'(
     }
